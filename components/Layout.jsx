00da712c829d8f6d8711fc3745dc5bf9eb865837
AnilMandavilli594 <61509925+AnilMandavilli594@users.noreply.github.com>
2023-12-05 21:11:20 -0500

    header-change

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JS Mastery Store</title>
+        <title>SimplyBuy</title>
       </Head>
       <header>
         <Navbar />
